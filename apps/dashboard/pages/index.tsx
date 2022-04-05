426a6841f7b7abbfa25dad3b8692ed9d496534f9
Alejandro Nanez <git@alejandro.dev>
2022-04-04 21:05:18 -0500

    feat: build pipeline with turbo

--- apps/dashboard/pages/index.tsx
+++ apps/dashboard/pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing this file{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
