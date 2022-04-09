59596733962a3582d638d91d59937340073d28cf
Alejandro Nanez <git@alejandro.dev>
2022-04-09 11:32:08 -0500

    test: fix tests

--- apps/blog/src/__test__/App.test.tsx
+++ apps/blog/src/__test__/App.test.tsx
@@ -9,7 +9,7 @@ describe('App', () => {
     render(<App />)
 
     const heading = screen.getByRole('heading', {
-      name: /hello https:\/\/google.io/i,
+      name: /hello https:\/\/egghead.io/i,
     })
 
     expect(heading).toBeInTheDocument()
